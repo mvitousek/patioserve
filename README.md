3b0cfafd492308dd645e5e4cf5f9266632fa3e0c
Mike Vitousek <mvitousek@fb.com>
2021-02-15 19:21:16 -0800

    fix to cacheing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
 # patioserve
 Simple customizable menu, to make it easier to show friends what you can bring them from inside in pandemic-era socially distanced gatherings.
+
+Made with Flow, React, and Next.js.
--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
-    runtimeCaching,
   },
 });
